ba10e3c163a28ac9da6b0d629167bd021870d6c9
Travis E. Oliphant <teoliphant@gmail.com>
2011-07-04 23:25:18 -0500

    Add new pypy-run files

--- laplace/laplace.py
+++ laplace/laplace.py
@@ -1,29 +1,25 @@
 from numpy import zeros, abs
 from scipy import weave
 import numexpr as ne
+from laplace import cy_update
 import pyximport
 import numpy as np
+from _laplace_for import for_update1, for_update2
+pyximport.install(setup_args={'include_dirs':[np.get_include()]})
+from _laplace import cy_update as cy_update2
+
 
 dx = 0.1
 dy = 0.1
 dx2 = dx*dx
 dy2 = dy*dy
 
-def dummy_update(u):
-    pass
 
-def py_update(u):
-    nx, ny = u.shape
-    for i in xrange(1,nx-1):
-        for j in xrange(1, ny-1):
-            u[i,j] = ((u[i+1, j] + u[i-1, j]) * dy2 +
-                      (u[i, j+1] + u[i, j-1]) * dx2) / (2*(dx2+dy2))
-
-def num_update(u):
+def num_update(u, dx2, dy2):
     u[1:-1,1:-1] = ((u[2:,1:-1]+u[:-2,1:-1])*dy2 + 
                     (u[1:-1,2:] + u[1:-1,:-2])*dx2) / (2*(dx2+dy2))
 
-def expr_update(u):
+def expr_update(u, dx2, dy2):
     bottom = u[2:,1:-1]
     top = u[:-2,1:-1]
     left = u[1:-1,2:]
@@ -31,7 +27,7 @@ def expr_update(u):
     u[1:-1,1:-1] = ne.evaluate("((bottom + top)*dy2 + "\
                     "(left + right)*dx2) / (2*(dx2+dy2))")
     
-def weave_update(u):
+def weave_update(u, dx, dy2):
     code = """
     int i, j;
     for (i=1; i<Nu[0]-1; i++) {
@@ -43,19 +39,35 @@ def weave_update(u):
     """
     weave.inline(code, ['u', 'dx2', 'dy2'])
 
-from laplace import cy_update
 
-import pyximport
-import numpy as np
-pyximport.install(setup_args={'include_dirs':[np.get_include()]})
-from _laplace import cy_update as cy_update2
-from _laplace_for import for_update1, for_update2
-
-def calc(N, Niter=100, func=py_update, args=()):
+def calc(N, Niter=100, func=num_update, args=()):
     order = 'C' if func not in [for_update1, for_update2] else 'F'
     u = zeros([N, N], order=order)
     u[0] = 1
     for i in range(Niter):
         func(u,*args)
     return u
+
+
+
+def main():
+    import time
+    N = 150
+    Niter = 8000
+    modes = [['NumPy', num_update, (dx2, dy2)],
+             ['Numexpr', expr_update, (dx2, dy2)],
+             ['Basic Cython', cy_update, (dx2, dy2)],
+             ['Faster Cython', cy_update2, (dx2, dy2)],
+             ['Weave', weave_update, (dx2, dy2)],
+             ['Looped Fortran', for_update1, (dx2, dy2, N, N)],
+             ['Vectorized Fortran', for_update2, (dx2, dy2, N, N)]
+             ]
+    for mode, update, args in modes:
+        start = time.time()
+        calc(N, Niter, func=update, args=args)
+        elapsed = time.time() - start
+        print "%s: %f seconds" % (mode, elapsed)
+
+if __name__ == '__main__':
+    main()
     
---
+++ laplace/run_commands.sh
@@ -0,0 +1,3 @@
+python laplace.py
+python laplace2.py
+pypy laplace2.py
